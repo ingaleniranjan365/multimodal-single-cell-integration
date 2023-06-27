0ecc50193cfcbc1097c88997bf46aee970d48db4
ingaleniranjan365 <ingaleniranjan365@gmail.com>
2023-06-27 09:39:14 +0530

    Updating script create_subset_data to select a subset of cells ids to work with and writing corresponding data (chromatin accessibility) as parquet which can be read into a pyspark.mllib.linalg.SparseMatrix instead of a sense matrix like before

--- ceate_subset_data.py
+++ ceate_subset_data.py
@@ -1,45 +1,88 @@
 import random
+from typing import List, Tuple
 
 import h5py
 import hdf5plugin
 import numpy as np
-import pandas as pd
+from joblib import Parallel, delayed
+from pyspark.sql import SparkSession
+from pyspark.ml.linalg import SparseMatrix
 
 SAMPLING_SIZE = 1000
 
-cell_ids_path_inputs = 'train_multi_inputs/axis1'
+
+def get_csc_values_for_matrix(matrix: np.ndarray) -> Tuple[int, int, List[int], List[int], List]:
+    row_cnt, column_cnt = matrix.shape
+
+    values = []
+    row_indices = []
+    column_pointers = [0]
+
+    def process_column(j):
+        non_zero_values = []
+        non_zero_indices = []
+        for i in range(row_cnt):
+            if matrix[i, j] != 0:
+                non_zero_values.append(matrix[i, j])
+                non_zero_indices.append(i)
+        return non_zero_values, non_zero_indices
+
+    non_zero_values_with_indices_for_all_columns = Parallel(n_jobs=-1)(
+        delayed(process_column)(j) for j in range(column_cnt))
+
+    for j in range(column_cnt):
+        non_zero_values, non_zero_indices = non_zero_values_with_indices_for_all_columns[j]
+        values.extend(non_zero_values)
+        row_indices.extend(non_zero_indices)
+        column_pointers.append(column_pointers[-1] + len(non_zero_values))
+
+    return row_cnt, column_cnt, column_pointers, row_indices, values
+
+
+def get_spark_session() -> SparkSession:
+    return SparkSession.builder \
+        .appName("multimodal-single-cell-integration") \
+        .config("spark.executor.memory", "4g") \
+        .config("spark.driver.memory", "2g") \
+        .getOrCreate()
+
+
+def write_subset_as_sparse_matrix_dataframe(
+        h5_file_path: str,
+        row_index_path: str,
+        matrix_path: str,
+        output_path: str,
+):
+    file = h5py.File(h5_file_path, 'r')
+    sampled_row_index = random.sample(range(0, file[row_index_path].shape[0]), SAMPLING_SIZE)
+    sampled_matrix = np.stack(
+        tuple([file[matrix_path][i, :] for i in sampled_row_index])
+    )
+
+    row_cnt, column_cnt, column_pointers, row_indices, values = get_csc_values_for_matrix(sampled_matrix)
+
+    values = np.array(values)
+    row_indices = np.array(row_indices)
+    column_pointers = np.array(column_pointers)
+
+    sparse_matrix = SparseMatrix(row_cnt, column_cnt, column_pointers, row_indices, values)
+
+    data = [(sparse_matrix.numRows, sparse_matrix.numCols, sparse_matrix.colPtrs.tolist(), sparse_matrix.rowIndices.tolist(),
+             sparse_matrix.values.tolist())]
+    get_spark_session().createDataFrame(
+        data, ["num_rows", "num_cols", "col_ptrs", "row_indices", "values"]
+    ).write.parquet(output_path)
+
+
+train_multi_inputs_path = '/Users/niranjani/code/multimodal-single-cell-integration/data/raw_data/train_multi_inputs.h5'
+cell_ids_path = 'train_multi_inputs/axis1'
 genomic_coordinates_path = 'train_multi_inputs/block0_items'
 chromatin_accessibility_matrix_path = 'train_multi_inputs/block0_values'
-train_multi_inputs_path = '/Users/niranjani/code/multimodal-single-cell-integration/data/raw_data/train_multi_inputs.h5'
-chromatin_accessibility_of_sample_cells_path = '/Users/niranjani/code/multimodal-single-cell-integration/data/subset/chromatin_accessibility.parquet'
+chromatin_accessibility_of_sample_cells_path = 'data/subset'
 
-cell_ids_path_targets = 'train_multi_targets/axis1'
-genes_path = 'train_multi_targets/block0_items'
-gene_expression_matrix_path = 'train_multi_targets/block0_values'
-train_multi_targets_path = '/Users/niranjani/code/multimodal-single-cell-integration/data/raw_data/train_multi_targets.h5'
-gene_expression_of_sample_cells_path = '/Users/niranjani/code/multimodal-single-cell-integration/data/subset/gene_expression.parquet'
-
-train_multi_inputs = h5py.File(train_multi_inputs_path, 'r')
-train_multi_targets = h5py.File(train_multi_targets_path, 'r')
-
-assert train_multi_inputs[cell_ids_path_inputs].shape[0] == train_multi_targets[cell_ids_path_targets].shape[0]
-
-random_cell_id_sample_index = random.sample(range(0, train_multi_inputs[cell_ids_path_inputs].shape[0]), SAMPLING_SIZE)
-
-chromatin_accessibility_of_sample_cells = np.stack(
-    tuple([train_multi_inputs[chromatin_accessibility_matrix_path][i, :] for i in random_cell_id_sample_index]))
-genomic_coordinates = train_multi_inputs[genomic_coordinates_path][:]
-genomic_coordinates_index = [str(num) for num in range(genomic_coordinates.shape[0])]
-
-df = pd.DataFrame(chromatin_accessibility_of_sample_cells, columns=genomic_coordinates_index)
-df.to_parquet(chromatin_accessibility_of_sample_cells_path)
-
-
-
-gene_expression_of_sample_cells = np.stack(
-    tuple([train_multi_targets[gene_expression_matrix_path][i, :] for i in random_cell_id_sample_index]))
-genes = train_multi_targets[genes_path][:]
-genes_index = [str(num) for num in range(genes.shape[0])]
-
-df = pd.DataFrame(gene_expression_of_sample_cells, columns=genes_index)
-df.to_parquet(gene_expression_of_sample_cells_path)
+write_subset_as_sparse_matrix_dataframe(
+    train_multi_inputs_path,
+    cell_ids_path,
+    chromatin_accessibility_matrix_path,
+    chromatin_accessibility_of_sample_cells_path,
+)
